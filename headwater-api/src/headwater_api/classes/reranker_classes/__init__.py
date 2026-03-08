cc80104400e24132a819981a957e12048d654237
acesanderson <brian.briananderson@gmail.com>
2026-03-07 20:41:28 -0600

    fix: add from __future__ import annotations to reranker_classes __init__

--- headwater-api/src/headwater_api/classes/reranker_classes/__init__.py
+++ headwater-api/src/headwater_api/classes/reranker_classes/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from headwater_api.classes.reranker_classes.requests import RerankDocument, RerankRequest
 from headwater_api.classes.reranker_classes.responses import (
     RerankResult,
